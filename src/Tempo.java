e471527aca322b39767ed94e682f9172a9560399
kaue-andrade <kauedd22@gmail.com>
2022-03-29 09:53:22 -0300

    ProjetoDoTempo

--- src/Tempo.java
+++ src/Tempo.java
@@ -1,42 +1,44 @@
-public class Tempo {
-    private int hora;
-    private int minuto;
-    private long segundo;
-    //private int soma;
-    private long converterhoras;
-    private long converterminutos;
+//Algoritmo continuado por Kauê Andrade dos Santos.
 
-    public Tempo() {
-        this.hora = 0;
-        this.minuto = 0;
-        this.segundo = 0;
+public class Tempo { //Cria a classe pública denominada Tempo
+
+    private int hora; //Cria a variável hora como tipo int e private
+    private int minuto; //Cria a variável minuto como tipo int e private
+    private long segundo; //Cria a variável segundo como tipo int e private
+    private long converterhoras; //Cria a variável converterhoras como tipo long e private
+    private long converterminutos; //Cria a variável converterminutos como tipo long e private
+
+    public Tempo() { //Primeiro método construtor
+        this.hora = 0; //this.minuto recebe 0
+        this.minuto = 0; //this.minuto recebe 0
+        this.segundo = 0; //this.segundo recebe 0
     }
 
-    public Tempo(int h) {
+    public Tempo(int h) { //Segundo método construtor
         if (validarTime(h, 0, 0)) {
-            this.hora = h;
-            this.minuto = 0;
-            this.segundo = 0;
+            this.hora = h; //this.hora recebe h (variável que está representando a hora)
+            this.minuto = 0; //this.minuto recebe 0
+            this.segundo = 0; //this.segundo recebe 0
         }
     }
 
-    public Tempo(int h, int m) {
+    public Tempo(int h, int m) { //Terceiro método construtor
         if (validarTime(h, m, 0)) {
-            this.hora = h;
-            this.minuto = m;
-            this.segundo = 0;
+            this.hora = h; //this.hora recebe h (está representando a hora)
+            this.minuto = m; //this.minuto recebe m (está representando o minuto)
+            this.segundo = 0; //this.segundo recebe 0
         }
     }
 
-    public Tempo(int h, int m, int s) {
-        if (validarTime(h, m, s)) {
-            this.hora = h;
-            this.minuto = m;
-            this.segundo = s;
+    public Tempo(int h, int m, int s) { //Terceito método construtor
+        if (validarTime(h, m, s)) { //Validação de tempo utilizando if (em português, se)
+            this.hora = h; //this.hora recebe h (está representando a hora)
+            this.minuto = m; //this.minuto recebe m (está representando o minuto)
+            this.segundo = s; //this.segundo recebe s (está representando o segundo)
         }
     }
 
-    public boolean validarTime(int h, int m, int s) {
+    public boolean validarTime(int h, int m, int s) { //Cria um método para validar o tempo
         if ((h >= 0 && h < 24) && (m >= 0 && m < 60) && (s >= 0 && s < 60)) {
             return true;
         } else {
@@ -44,31 +46,95 @@ public class Tempo {
         }
     }
 
-    public String toString() {
-        String modelo = "";
+    public String toString() { //Cria o método toString
 
-        if (hora < 9) {
-            modelo += "0" + hora + ":";
-        } else modelo += hora + ":";
-        if(minuto < 9){
+        String modelo = ""; //O modelo do relógio recebe ""
+
+        if (hora < 9) { //Estebelece a condição de hora < 9
+            modelo += "0" + hora + ":"; //Se hora menor que 9,
+        } else {
+            modelo += hora + ":";
+        }
+        if (minuto < 9){
             modelo += "0" + minuto + ":";
-        } else modelo += minuto + ":";
+        } else {
+            modelo += minuto + ":";
+        }
         if (segundo < 9 ){
-            modelo += "0"+segundo;
-        }else modelo += segundo;
+            modelo += "0"+ segundo;
+        } else {
+            modelo += segundo;
+        }
 
-        return modelo;
+        return modelo; //Retorna o modelo do relógio
     }
 
-    public long tempoSegundos(){
-        converterhoras = hora*60;
-        converterminutos = minuto*60;
-        //soma = converterhoras + segundo + minuto;
-        segundo = segundo + (converterhoras * 60) + converterminutos;
-        return segundo;
+    public long tempoSegundos(){ //Método para transformar o tempo informado em segundos
+        converterhoras = hora*60; //Converte as horas em minutos multiplicando por 60
+        converterminutos = minuto*60; //Converte as minutos em segundo multiplicando por 60
+        segundo = segundo + (converterhoras * 60) + converterminutos; //Armazena a soma entre segundo, a conversão de horas em segundos e a conversão de minutos em segundos na variável segundo
+        return segundo; //Retorna a variável segundo
     }
+
+    public boolean setHora(int hora) {
+
+        //Uma certa hora, para ser válida, necessita ser maior ou igual a 0 e menor ou igual a 24
+
+        if ((hora >= 0) && (hora <= 24)) { //Estabelece a condição "se" de hora >= 0 e hora <= 24
+            this.hora = hora; //this.hora recebe hora
+            return true; //Se a condição for atendida, returna "true" (verdadeiro em português)
+        } else { //Estabelece a condição de "senão", caso a do "se" não seja atendida
+            this.hora = 0; //this.hora recebe o valor 0
+            return false; //Se a condição for atendida, retorna "false" (falso em português)
+        }
+    }
+
+    //Um certo minuto, para ser válido, necessita ser maior ou igual a 0 e menor ou igual a 59
+
+    public boolean setMinutos(int minuto) { //Estabelece a condição "se" de minuto >= 0 e minuto <= 24
+
+        if ((minuto >= 0) && (minuto <= 59)) { //
+            this.minuto = minuto; //this.minuto recebe minuto
+            return true; //Se a condição for atendida, retorna "true" (verdadeiro em português)
+        } else { //Estabelece a condição de "senão", caso a condição do "se" não seja atendida
+            this.minuto = 0; //this.hora recebe o valor 0
+            return false; //Se a condição for atendida, retorna "false" (falso em português)
+        }
+    }
+
+    public boolean setSegundos(int segundo){
+        if((segundo >= 0) && (segundo <= 59)){
+            this.segundo = segundo; //this.segundo recebe segundo
+            return true; //Se a condição for atendida,
+        } else { //Estabelece a condição de "senão", caso a condição do "se" não seja atendida
+            this.segundo = 0; //this.segundo recebe o valor 0
+            return false; //Se a condição for atendida, retorna "false" (falso em português)
+        }
+    }
+
+
+
+
+
+    /*public long diferencaTempo(Tempo t2){
+        /*long tempo1 = t2;
+        System.out.println("\ntempo1" + tempo1);
+        System.out.println("\nTEMPO: " + tempo);
+        long temponovo = tempoSegundos();
+        System.out.println("TEMPO NOVO: " + tempo);
+        long tempo1 = t1.tempoSegundos();
+        long tempo2 = t2.tempoSegundos();
+        System.out.println("\ntempo2" + tempo2);
+        long diferenca = tempo1-tempo2;
+        return diferenca;
+    }*/
+
+
     //public void alterarHora(int h, int m, int s){
 
+
+
+    //Geração dos getters e setters
 
     public int getHora() {
         return this.hora;
@@ -81,8 +147,24 @@ public class Tempo {
     public long getSegundo() {
         return segundo;
     }
-    //Métodos que serão criados...
 
+    public long getConverterhoras() {
+        return converterhoras;
+    }
+
+    public void setConverterhoras(long converterhoras) {
+        this.converterhoras = converterhoras;
+    }
+
+    public long getConverterminutos() {
+        return converterminutos;
+    }
+
+    public void setConverterminutos(long converterminutos) {
+        this.converterminutos = converterminutos;
+    }
+
+    //Métodos que serão criados...
 
     //5ª questão
     //public long compare(Tempo t){
--- src/TempoMain.java
+++ src/TempoMain.java
@@ -1,16 +1,47 @@
-public class TempoMain {
-    public static void main(String[] args) {
-        Tempo t1 = new Tempo(14, 21, 39);
-        System.out.println("\nHORA(S): " + t1.getHora() + " hora(s)");
-        System.out.println("MINUTO(S): " + t1.getMinuto() + " minuto(s)");
-        System.out.println("SEGUNDOS: " + t1.getSegundo() + " segundo(s)");
+//Algoritmo continuado por Kauê Andrade dos Santos.
 
-        System.out.println("\nHORÁRIO EM STRING: " + t1.toString());
+public class TempoMain { //Cria a classe pública denominada TempoMain
+    public static void main(String[] args) { //public: modificador de acesso do método público; static: estático; void: sem retorno; main (principal): nome do método; (String[] args): receber como parâmetro um array de String (denominado args).
 
-        System.out.println("\nCONVERSÃO PARA SEGUNDOS: " + t1.tempoSegundos() + " segundo(s)");
+        Tempo t1 = new Tempo(01, 00, 00); //Criação do t2 e estabelecimento dos valores
+        System.out.println("#Tempo 1"); //Exibe na tela #TEMPO 1
+        System.out.println("\nHORA(S): " + t1.getHora() + " hora(s)"); //Exibe a hora de t1
+        System.out.println("MINUTO(S): " + t1.getMinuto() + " minuto(s)"); //Exibe o(s) minuto(s) de t1
+        System.out.println("SEGUNDOS: " + t1.getSegundo() + " segundo(s)"); //Exibe o(s) segundo(s) de t1
 
+        System.out.println("\nHORÁRIO EM STRING: " + t1.toString()); //Exibe o horário de t1 em string
+
+        System.out.println("\nCONVERSÃO PARA SEGUNDOS: " + t1.tempoSegundos() + " segundo(s)\n"); //Exibe a conversão do tempo em segundos de t1
+
+        if (t1.setHora(15)){
+            System.out.println("Você definiu a hora com o valor um valor válido"); //Exibe a mensagem se a condição for atendida
+        } else{
+            System.out.println("Ops! Você definiu uma hora inválida! O valor recebe 0. Lembre-se: a hora precisa estar entre 0 e 24"); //Exibe a mensagem se a condição for atendida
+        }
+
+        if (t1.setMinutos(16)){
+            System.out.println("Você definiu os minutos com um valor válido"); //Exibe a mensagem se a condição for atendida
+        } else{
+            System.out.println("Ops! Você definiu os minutos com um valor inválido! O valor recebe 0. Lembre-se: os minutos precisam estar entre 0 e 59"); //Exibe a mensagem se a condição for atendida
+        }
+
+        if (t1.setSegundos(60)) {
+            System.out.println("Você definiu os segundos com um valor válido"); //Exibe a mensagem se a condição for atendida
+        } else {
+            System.out.println("Ops! Você definiu os segundos com um valor inválido! O valor recebe 0. Lembre-se: os segundos precisam estar entre 0 e 59"); //Exibe a mensagem se a condição for atendida
+        }
+
+        System.out.println("\nNOVO HORÁRIO DE TEMPO 1 FOI DEFINIDO COMO: " + t1.toString());
+
+        Tempo t2 = new Tempo(02, 00, 00); //Criação do t2 e estabelecimento dos valores
+
+        System.out.println("\n#TEMPO 2"); //Exibe na tela "#TEMPO 2"
+        System.out.println("\nSEGUNDO HORÁRIO: " + t2.toString()); //Exibe o horário de t2 em string
+        System.out.println("\nCONVERSÃO DO SEGUNDO HORÁRIO PARA SEGUNDOS: " + t2.tempoSegundos() + " segundo(s)");
+        //System.out.println("\nDIFERENÇA ENTRE OS DOIS HORÁRIOS: " + t1.diferencaTempo(t1, t2) + " segundo(s)");
         //5ª questão
         //Tempo t2 = new Tempo(15, 23, 32);
         //t2.compare(t1);
+
     }
 }
